2522ebd5f94c11237ba12393701ad5fa215ef94c
Sam <samrob.nz@gmail.com>
2019-07-25 15:10:40 +1200

    react2

--- Apps/note-app/src/App.js
+++ Apps/note-app/src/App.js
@@ -6,56 +6,62 @@ class App extends Component {
 
   constructor(props){
     super(props);
-  }
+      this.notes = [
+        {
+          id:1,
+          text: 'Washing'
+        },
+        {
+          id:2,
+          text: 'Cook Dinner'
+        },
+        {
+          id:3,
+          text: 'React'
+        },
+      ];
+    }
 
   render(){
     return (
       <div className="wrap">
         <div className="container">
           <div className="notes">
-            <div className="note">
-              <div className="note-body">
-                <i className="far fa-times-circle note-remove"></i>
-                <div className="note-text">
-                  Get milk
-                </div>
-              </div>
-            </div>
-            <div className="note">
-              <div className="note-body">
-                <i className="far fa-times-circle note-remove"></i>
-                <div className="note-text">
-                  Water the plants
-                </div>
-              </div>
-            </div>
-    
-            <div className="note">
-              <div className="note-body">
-                <i className="far fa-times-circle note-remove"></i>
-                <div className="note-text">
-                  Ring Peter
-                </div>
-              </div>
-            </div>
-        
-            <div className="note new-note">
-    
-              <form className="note-body">
+                {
+                  this.notes.map(function(note){
+                    return(
 
-                  <div className="form-group">
-                    <label for="note-input">New note</label>
-                    <input type="text" className="form-control" id="note-input"/>
-                  </div>
-            
-                  <button type="submit" className="btn btn-primary">Add</button>
-              </form>
-              
+                     <div className="note" key={note.id}>
+                      <div className="note-body">
+                        <i className="far fa-times-circle note-remove"></i>
+                        <div className="note-text">
+                          {note.text}
+                        </div>
+                      </div>
+                    </div>
+
+                    );
+                  })
+                }
+
+                <div className="note new-note">
+                    
+                  <form className="note-body">
+
+                      <div className="form-group">
+                        <label for="note-input">New note</label>
+                        <input type="text" className="form-control" id="note-input"/>
+                      </div>
+                
+                      <button type="submit" className="btn btn-primary">Add</button>
+                  </form>
+                  
+                </div>
+
             </div>
           </div>
-        </div>
-     </div>
 
+        </div>
     );
   }
 
