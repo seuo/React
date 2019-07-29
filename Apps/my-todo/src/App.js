334e77e5d4090a22f9e8ac0e02fd55bd3c852525
Sam <samrob.nz@gmail.com>
2019-07-29 13:06:30 +1200

    first mytodo uni

---
+++ Apps/my-todo/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Note from './Note';
+import './App.css';
+
+
+class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        noteText: '',
+        notes: [],
+      }
+    }
+
+    updateNoteText(noteText) {
+        this.setState({ noteText: noteText.target.value})  // updates note text onChangeText noteText => this
+    }
+
+    addNote() {
+      if (this.state.noteText === ''){return}
+
+        let notesArr = this.state.notes;
+        notesArr.push(this.state.noteText);
+        this.setState({ noteText: ''});
+        this.textInput.focus();
+    }
+
+    handleKeyPress = (event) => {
+      if (event.key === 'Enter') { // add enter key on input
+        let notesArr = this.state.notes;
+        notesArr.push(this.state.noteText);
+        this.setState({ noteText:''});
+      }
+    }
+
+    deleteNote(index) {
+      let notesArr = this.state.notes;
+      notesArr.splice(index, 1);
+      this.setState({notes:notesArr})
+    }
+
+
+
+    render(){
+
+      let notes = this.state.notes.map((val, key) => {
+        return <Note key={key} text={val}
+                deleteMethod={ () => this.deleteNote(key) } />
+      })
+
+
+      
+
+      return (
+        <div className="container">
+          <div className="header">My Todo List</div>
+          {notes}
+
+          <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+          
+          <input type="text"
+            ref={((input) => {this.textInput = input})}
+            className = "textInput"
+            value={this.state.noteText}
+            onChange={noteText => this.updateNoteText(noteText)} 
+            onKeyPress={this.handleKeyPress.bind(this)} // bind keypress fron handleKeyPress event
+            />
+
+        </div>
+    );
+}
+}
+
+export default App;
